6a0a88f9e6b665579fa64ad2fa64f5dc1bd73e4b
Iyaan Azeez <iyaanazeez757@gmail.com>
2024-09-10 13:14:43 +0500

    Set memory locations and ranges with values.

--- interpreter/interpreter.ts
+++ interpreter/interpreter.ts
@@ -49,7 +49,7 @@ const prompt = ">>> ";
 process.stdout.write(prompt);
 for await (const line of console) {
   // console.log(`You typed: ${line}`);
-  if (line == "print registers") {
+  if (line == "print reg") {
     // Print contents of registers
     const {
       accumulator,
@@ -70,7 +70,7 @@ IR  : ${instructionRegister}
 IBR : ${instructionBufferRegister.op} ${instructionBufferRegister.addr}`;
 
     console.log(output);
-  } else if (line.includes("print memory")) {
+  } else if (line.includes("print mem")) {
     let lineArr = line.split(" ");
     let lowerBound = DISPLAY_MEMORY_DEFAULT_MIN;
     let higherBound = DISPLAY_MEMORY_DEFAULT_MAX;
@@ -90,14 +90,91 @@ IBR : ${instructionBufferRegister.op} ${instructionBufferRegister.addr}`;
     console.table(machine.getMemoryByRange(lowerBound, higherBound));
   } else if (line.startsWith("reset")) {
     // Clear memory and registers (RESET)
-    if (line.includes("registers")) {
+    if (line.includes("reg")) {
       machine.resetRegisters();
       console.log("Cleared register values");
     } else {
       machine.resetEmulator();
       console.log("Cleared register and memory");
     }
+  } else if (line.startsWith("set mem")) {
+    // Set memory segments to a value (set memory [index] [rop] [raddr] [lop] [laddr])
+    const cmdArr = line.split(" ");
+    if (cmdArr.length !== 7) {
+      console.log("Not enough arguments");
+      console.log("help: set mem [index] [rop] [raddr] [lop] [laddr]");
+    } else {
+      const index = cmdArr[2];
+      let rop = parseStringToNumber(cmdArr[3]);
+      let raddr = parseStringToNumber(cmdArr[4]);
+      let lop = parseStringToNumber(cmdArr[5]);
+      let laddr = parseStringToNumber(cmdArr[6]);
+
+      rop = !rop ? 0 : rop;
+      raddr = !raddr ? 0 : raddr;
+      lop = !lop ? 0 : lop;
+      laddr = !laddr ? 0 : laddr;
+
+      const { lowerBound, higherBound } = parseRange(index);
+      if (!lowerBound || !higherBound) {
+        console.log("Could not parse range!");
+        console.log("help: set mem [0:5] [rop] [raddr] [lop] [laddr]");
+      } else if (lowerBound === -1) {
+        machine.memory[higherBound] = new Instruction(rop, raddr, lop, laddr);
+      } else {
+        for (let i = lowerBound; i < higherBound; i++) {
+          machine.memory[i] = new Instruction(rop, raddr, lop, laddr);
+        }
+      }
+    }
+  } else {
+    // Parse and run instructions (Default)
+    scanner.source = line;
+    try {
+      scanner.scanTokens();
+      const tokens = scanner.tokens;
+      parser.readTokens(tokens);
+      parser.parse();
+      const instructions = parser.instructions;
+      if (instructions.length !== 1) {
+        console.error("Could not read instruction from REPL");
+        continue;
+      }
+      machine.loadInstructionREPL(instructions[0]);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   process.stdout.write(prompt);
+}
+
+function parseStringToNumber(str: string) {
+  const value = parseInt(str);
+  if (value === undefined || Number.isNaN(value)) {
+    console.log(`Cannot parse number. Value: ${str}`);
+    return null;
+  }
+  return value;
+}
+
+// Parses arguments like 8:19. Returns -1 if cannot parse a range
+function parseRange(str: string) {
+  let higherBound = -1;
+  let lowerBound = -1;
+  const tempArr = str.split(":");
+  if (tempArr.length == 2) {
+    lowerBound = parseInt(tempArr[0]);
+    higherBound = parseInt(tempArr[1]);
+  } else if (tempArr.length == 1) {
+    higherBound = parseInt(tempArr[0]);
+  }
+
+  if (lowerBound > higherBound) {
+    console.log(
+      `Range incorrect. lower > higher. ${lowerBound}:${higherBound}`
+    );
+  }
+
+  return { lowerBound, higherBound };
 }
